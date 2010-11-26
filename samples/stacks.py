5cff86598cb3c27de67d91d51d635fe4b1e9033e
SND\kernelnet_cp <SND\kernelnet_cp@9b283d60-5439-405e-af05-b73fd8c4d996>
2010-11-26 11:53:20 +0000

    [!] re-commit
    git-svn-id: https://pykd.svn.codeplex.com/svn@58075 9b283d60-5439-405e-af05-b73fd8c4d996

--- samples/stacks.py
+++ samples/stacks.py
@@ -2,13 +2,6 @@ import sys
 from pykd import *
 
 
-#def loadSymbols():
-#
-#    global nt
-#    nt = loadModule( "nt" )
-#    nt.PsActiveProcessHead = getOffset( "nt", "PsActiveProcessHead" )
-
-
 def printStack():
 
     threadList = getThreadList()
@@ -19,11 +12,6 @@ def printStack():
         stackFrames = getCurrentStack()
         for frame in stackFrames: dprintln( findSymbol( frame.instructionOffset ) + "  (%x)" % frame.instructionOffset )
         dprintln("")
-
-
-#    stackFrames = getCurrentStack()
-	
-#    [ dprintln( findSymbol( f.instructionOffset ) + "  (%x)" % f.instructionOffset )  for f in stackFrames ]
 
 
 if __name__ == "__main__":
