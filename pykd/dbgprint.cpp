06fb9c267a022a295499fca7b2333ded765facdc
SND\kernelnet_cp <SND\kernelnet_cp@9b283d60-5439-405e-af05-b73fd8c4d996>
2011-03-05 15:08:41 +0000

    [!] fixed : issue 8469 ( dprintln does not work in console mode )
    git-svn-id: https://pykd.svn.codeplex.com/svn@62227 9b283d60-5439-405e-af05-b73fd8c4d996

--- pykd/dbgprint.cpp
+++ pykd/dbgprint.cpp
@@ -11,7 +11,9 @@ void DbgPrint::dprint( const boost::python::object& obj, bool dml )
 {
     std::wstring   str = boost::python::extract<std::wstring>( obj );
 
-    dbgExt->control4->ControlledOutputWide(  dml ? DEBUG_OUTCTL_AMBIENT_DML : DEBUG_OUTCTL_AMBIENT_TEXT, DEBUG_OUTPUT_NORMAL, str.c_str() );
+    HRESULT  hres = dbgExt->control4->ControlledOutputWide(  dml ? DEBUG_OUTCTL_AMBIENT_DML : DEBUG_OUTCTL_AMBIENT_TEXT, DEBUG_OUTPUT_NORMAL, str.c_str() );
+   
+    std::wcout << str;
 }
 
 void DbgPrint::dprintln( const boost::python::object& obj, bool dml  )
@@ -20,5 +22,7 @@ void DbgPrint::dprintln( const boost::python::object& obj, bool dml  )
     str += L"\r\n";
 
     dbgExt->control4->ControlledOutputWide(  dml ? DEBUG_OUTCTL_AMBIENT_DML : DEBUG_OUTCTL_AMBIENT_TEXT, DEBUG_OUTPUT_NORMAL, str.c_str() );
+    
+    std::wcout << str;    
 }
 
