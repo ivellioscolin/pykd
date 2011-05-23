a1f173a1939c0999491a48395325afbcf3a60024
SND\kernelnet_cp <SND\kernelnet_cp@9b283d60-5439-405e-af05-b73fd8c4d996>
2011-05-23 09:23:53 +0000

    [pykd] added : lost dbgbreak.cpp/h files
    git-svn-id: https://pykd.svn.codeplex.com/svn@65723 9b283d60-5439-405e-af05-b73fd8c4d996

---
+++ pykd/dbgbreak.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include <map>
+#include "dbgext.h"
+
+/////////////////////////////////////////////////////////////////////////////////
+
+class  dbgBreakpointClass {
+
+public:
+
+    dbgBreakpointClass( ULONG64 offset, boost::python::object  &callback );
+    
+    ~dbgBreakpointClass();
+    
+    bool
+    set();
+    
+    void
+    remove();    
+
+    std::string
+    print() const;
+
+private:
+
+    ULONG64                 m_offset;
+
+    IDebugBreakpoint        *m_breakpoint;
+    
+    boost::python::object   m_callback;
+    
+private:
+
+    typedef std::map<IDebugBreakpoint*, dbgBreakpointClass*>     breakpointMap;
+    static  breakpointMap       m_breakMap;
+    
+public:    
+    
+    static HRESULT onBreakpointEvnet( IDebugBreakpoint*  bp );
+    
+};
+
+/////////////////////////////////////////////////////////////////////////////////
+
