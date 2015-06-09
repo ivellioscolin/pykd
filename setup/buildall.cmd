8eab57aa3c8d789e6dcdf7208d86b7a661f3c506
SND\kernelnet_cp <SND\kernelnet_cp@9b283d60-5439-405e-af05-b73fd8c4d996>
2015-06-09 08:26:48 +0000

    [0.3.x] updated : build scripts
    git-svn-id: https://pykd.svn.codeplex.com/svn@90667 9b283d60-5439-405e-af05-b73fd8c4d996

---
+++ setup/buildall.cmd
@@ -0,0 +1,4 @@
+python setup.py bdist_zip --plat-name=win32
+python setup.py bdist_zip --plat-name=win-amd64
+python setup.py bdist_wheel --plat-name=win32
+python setup.py bdist_wheel --plat-name=win-amd64
